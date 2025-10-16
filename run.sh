759291c9d55ccf4daec1edcf01de15b785a33878
daniilmars <daniilmarszallek@yahoo.de>
2025-10-16 22:47:07 +0200

    chore(debug): replace main app with test loop

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-echo "--- Starting Crypto Bot ---"
-set -e
-
-# Start the main application
-exec python3 main.py "$@"
+while true; do
+  echo "--- Container is running ---"
+  sleep 10
+done
